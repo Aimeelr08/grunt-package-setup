4f8049cab2e5a8bc566f8398ffcae632d20f99d3
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-09-01 22:24:02 -0400

    adding the explaination to use this template

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+
 app.listen(process.env.PORT);
+//app.listen(3000); or run PORT=1234 node server.js
 console.log("Listening on port: " + process.env.PORT);
